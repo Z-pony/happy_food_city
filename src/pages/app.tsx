3efe7a32eca46558e5fdef59d54c46e34621e1fe
zpony <877473043@qq.com>
2020-07-31 17:17:36 +0800

    样式修改

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
-import {
-    BrowserRouter, Route, HashRouter, Switch
-} from 'react-router-dom';
 import ContentRoutes from 'src/routes/index';
-import Publish from 'src/pages/publish/index';
 
 export default class App extends React.Component {
   onChange = () => { };
@@ -13,19 +9,12 @@ export default class App extends React.Component {
           <div className="zpony_city">
               <div className="zpony_nav">
                   <nav className="zpony_nav_wrap">
+                      <Link to="/a">组件A</Link>
 
                   </nav>
               </div>
               <div className="zpony-wrap">
                   <ContentRoutes />
-                  {/* <Switch>
-                      <Route
-                          component={Publish}
-                          exact
-                          path="/publish"
-                      />
-
-                  </Switch> */}
               </div>
           </div>
       );
