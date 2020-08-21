0dd42abf58ec7851c04e505ee3c71f5cf9eae18b
zpony <877473043@qq.com>
2020-08-21 17:33:28 +0800

    提交样式

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,32 +1,38 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ContentRoutes from 'src/routes/index';
 import routerConfig from 'src/routes/config';
 
-export default class App extends React.Component {
-  onChange = () => { };
+export default function App(props) {
+    const [ isLogin, setIsLogin ] = useState(false);
 
-  render() {
-      return (
-          <div className="zpony_city">
-              <div className="zpony_nav">
-                  <nav className="zpony_nav_wrap clearfix">
-                      <ul className="zpony_nav_list fl clearfix">
-                          {
-                              routerConfig().map((item) => !!item.show && (
-                                  <li className="zpony_nav-item fl" key={item.link}><Link to={item.link}>{item.title}</Link></li>
-                              ))
-                          }
-                      </ul>
-                      <div className="fr">
-
-                      </div>
-                  </nav>
-              </div>
-              <div className="zpony-wrap">
-                  <ContentRoutes />
-              </div>
-          </div>
-      );
-  }
+    useEffect(() => {
+        console.log('1111');
+        console.log(props);
+    });
+    return (
+        <div className="zpony_city">
+            <div className="zpony_nav">
+                <nav className="zpony_nav_wrap clearfix">
+                    <ul className="zpony_nav_list fl clearfix">
+                        {
+                            routerConfig().map((item) => !!item.show && (
+                                <li className="zpony_nav_item fl" key={item.link}><Link to={item.link}>{item.title}</Link></li>
+                            ))
+                        }
+                    </ul>
+                    <div className="fr zpony_login_box">
+                        {
+                            isLogin ? <span>未登录</span> : (
+                                <span className="zpony_login_btn">登录</span>
+                            )
+                        }
+                    </div>
+                </nav>
+            </div>
+            <div className="zpony_wrap">
+                <ContentRoutes />
+            </div>
+        </div>
+    );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,35 +1,32 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
     Switch, Route
 } from 'react-router-dom';
 import asyncComponent from 'src/routes/asyncComponent';
 import routerConfig from 'src/routes/config';
 
+export default function ContentRoutes() {
+    return (
+        <Switch>
+            {
+                routerConfig().map((item) => !!item.show && (
+                    <Route
+                        key={item.link}
+                        exact
+                        component={asyncComponent(item.component)}
+                        path={item.link}
+                    />
+                ))
+            }
+            <Route
 
-export default class ContentRoutes extends Component {
-    render() {
-        return (
-            <Switch>
-                {
-                    routerConfig().map((item) => !!item.show && (
-                        <Route
-                            key={item.link}
-                            exact
-                            component={asyncComponent(item.component)}
-                            path={item.link}
-                        />
-                    ))
-                }
-                <Route
-
-                    exact
-                    component={asyncComponent(() => import('src/pages/notFound'))}
-                    path="/*"
-                />
-            </Switch>
-        );
-    }
+                exact
+                component={asyncComponent(() => import('src/pages/notFound'))}
+                path="/*"
+            />
+        </Switch>
+    );
 }
 
 
