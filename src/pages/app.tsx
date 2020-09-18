6d399b4e81c8a933f2643e16ca8a0627dd514427
zpony <877473043@qq.com>
2020-09-18 17:08:38 +0800

    路由跳转

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -2,13 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ContentRoutes from 'src/routes/index';
 import routerConfig from 'src/routes/config';
+import cs from 'classnames';
 
 export default function App(props) {
     const [ isLogin, setIsLogin ] = useState(false);
+    const [ path, setPath ] = useState('/');
 
     useEffect(() => {
-        console.log('1111');
-        console.log(props);
+        const linkArr: string[] = routerConfig().map((item) => item.link);
+        const pathName: string = props.history.location.pathname;
+
+        if (linkArr.indexOf(pathName) > -1) {
+            console.log(pathName);
+            setPath(pathName);
+        } else {
+            setPath('/');
+            props.history.push('/');
+        }
     });
     return (
         <div className="zpony_city">
@@ -17,7 +27,18 @@ export default function App(props) {
                     <ul className="zpony_nav_list fl clearfix">
                         {
                             routerConfig().map((item) => !!item.show && (
-                                <li className="zpony_nav_item fl" key={item.link}><Link to={item.link}>{item.title}</Link></li>
+                                <li
+                                    className={
+                                        cs({
+                                            'zpony_nav_item': true,
+                                            'fl': true,
+                                            'is_checked':  item.link === path
+                                        })
+                                    }
+                                    key={item.link}
+                                >
+                                    <Link to={item.link}>{item.title}</Link>
+                                </li>
                             ))
                         }
                     </ul>
